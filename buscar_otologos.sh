be20de3d53c4fd2aecc7876c77fb6d9474e1f0bf
alepl <43651749+alepl@users.noreply.github.com>
2018-10-04 12:29:36 -0500

    Add files via upload

---
+++ buscar_otologos.sh
@@ -0,0 +1,11 @@
+#esta linea es para cambiar el formato de excel de mac a linux
+perl -p -i -e 's/\r/\n/' $1 
+#ciclo para buscar lineas de un archivo en otro archivo
+#$1 son las lineas que quiero buscar en otro archivo y $2 es el archivo EN donde voy a buscar 
+for columna in $(cat $1)
+do
+echo $columna 
+grep $columna $2 >> $1-$2-ortologos
+
+
+done
